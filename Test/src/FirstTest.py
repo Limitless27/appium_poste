1f6642a58e3324b1baba4d9112e81f0ea8b02ebb
grazia.disomma <grazia.disomma@consulenti.mediolanum.it>
2022-01-20 16:29:04 +0100

    Project Appium - Test Case Funzionante!

--- Test/src/FirstTest.py
+++ Test/src/FirstTest.py
@@ -1,33 +1,28 @@
-from appium import webdriver
-from appium.webdriver.appium_service import AppiumService
-import time
+import unittest
+from unittest import TestCase
 
-from selenium.webdriver.common.by import By
+from method_object import Methods
 
-desired_cap = {
-    "platformName": "Android",
-    "appium:deviceName": "Android Emulator",
-    "appium:appPackage": "com.posteitaliane.spim",
-    "appium:appActivity": ".MainActivity"
-}
-# FIle di properties
-appium_service = AppiumService()
+class Test(TestCase):
 
-appium_service.start(args=['--address', '127.0.0.1', '--port', '4723'])
-assert (appium_service.is_running)
-assert (appium_service.is_listening)
-print("The server is running?"+ str(appium_service.is_running))
-print(appium_service.is_listening)
-driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_cap)
-while True:
-    try:
-        buttonChiudi = driver.find_element(By.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.TextView')
-        buttonChiudi.click()
-        buttonPacco = driver.find_element(By.XPATH, '//androidx.recyclerview.widget.RecyclerView[@content-desc="Le tue operazioni"]/android.widget.RelativeLayout[2]/android.widget.RelativeLayout/android.widget.TextView')
-        buttonPacco.click()
-        time.sleep(1)
-        appium_service.stop()
-    except ValueError:
-        print(" ERROR BRO  ")
-        pass
-    break
+    def test_1(self):
+        try:
+            methods = Methods()
+            print(methods.desired_cap)
+            desired = methods.details()
+            methods.AppiumServiceStart(desired)
+            methods.appium_service.stop()
+        except:
+            print()
+    def test_2(self):
+        try:
+            methods = Methods()
+            print(methods.desired_cap)
+            desired = methods.details()
+            methods.AppiumServiceStart(desired)
+            methods.appium_service.stop()
+        except:
+            print()
+
+if __name__ == '__main__':
+    unittest.main()
