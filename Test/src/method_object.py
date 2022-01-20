63d0fb817c9b7775070484d31ba840979f51718d
grazia.disomma <grazia.disomma@consulenti.mediolanum.it>
2022-01-20 16:28:12 +0100

    Project Appium - Test Case Funzionante!

---
+++ Test/src/method_object.py
@@ -0,0 +1,24 @@
+from appium import webdriver
+from appium.webdriver.appium_service import AppiumService
+# Define the class
+
+class Methods:
+    desired_cap = {}
+    appium_service = AppiumService()
+    # Define the method
+
+    def details(self):
+        desired_cap = {
+            "platformName": "Android",
+            "appium:deviceName": "Android Emulator",
+            "appium:appPackage": "com.posteitaliane.spim",
+            "appium:appActivity": ".MainActivity"
+        }
+        return desired_cap
+    def AppiumServiceStart(self, desired_cap):
+        appium_service = self.appium_service
+        appium_service.start(args=['--address', '127.0.0.1', '--port', '4729'])
+        assert (appium_service.is_running)
+        assert (appium_service.is_listening)
+        # start application on device
+        driver = webdriver.Remote('http://127.0.0.1:4729/wd/hub', desired_cap)
